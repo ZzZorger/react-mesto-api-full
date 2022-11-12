529e6932d4ccb766b587d25884461324dd9c9164
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-11-12 18:37:29 +0300

    Добавление cors middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,6 +8,8 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports.cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
+  console.log(req)
+  console.log(req.headers)
 
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
