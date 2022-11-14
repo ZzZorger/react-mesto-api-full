31d9c3e36933b8fa7b007e457d38498694640fed
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-11-14 21:38:15 +0300

    Добавление cors middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
-  'http://myfirstdomainand.nomoredomains.icu',
-  'https://myfirstdomainand.nomoredomains.icu',
-  'http://localhost:3000',
+  'http://myfirstdomainand.nomoredomains.icu/',
+  'https://myfirstdomainand.nomoredomains.icu/',
+  'http://localhost:3000/',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
@@ -11,7 +11,7 @@ module.exports.cors = (req, res, next) => {
 
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.header('Access-Control-Allow-Origin', origin);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
