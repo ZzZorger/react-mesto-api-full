a935f2fce7002369e450e55ec81000c1d0f93807
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-11-14 19:18:26 +0300

    Добавление cors middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,5 +1,6 @@
 const allowedCors = [
-  'http://myfirstdomainand.nomoredomains.icu/',
+  'http://myfirstdomainand.nomoredomains.icu/users/me',
+  'http://myfirstdomainand.nomoredomains.icu/cards',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
