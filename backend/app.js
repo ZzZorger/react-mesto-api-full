2194eb6ab6d52b8fc1bcc1e042b68554f12ea085
Alexandr Yakushkin <yalex007@mail.ru>
2022-11-14 14:57:40 +0300

    Настройка cors сервера

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
-const { cors } = require('./middlewares/cors');
+// const { cors } = require('./middlewares/cors');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
@@ -17,7 +17,30 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const { PORT = 3000 } = process.env;
 const app = express();
 // app.use(cors());
-app.use(cors);
+// app.use(cors);
+const allowedCors = [
+  'http://myfirstdomainand.nomoredomains.icu/',
+  'https://myfirstdomainand.nomoredomains.icu/',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+app.use(function(req, res, next) {
+  const { method } = req;
+  const { origin } = req.headers;
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    return res.end();
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
+// cors end
 const linkRegexp = /http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\./;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
