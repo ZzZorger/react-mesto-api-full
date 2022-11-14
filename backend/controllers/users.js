a8a585dc3ab1ae624a5aac87db240f56274bb59e
Alexandr Yakushkin <yalex007@mail.ru>
2022-11-14 15:36:08 +0300

    Настройка cors сервера

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -98,6 +98,7 @@ module.exports.updateAvatar = (req, res, next) => {
     });
 };
 module.exports.login = (req, res, next) => {
+  console.log(req.headers)
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
