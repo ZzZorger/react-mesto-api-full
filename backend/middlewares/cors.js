1fe71ee8765283d267d783eb73c8f836f8376770
Alexandr Yakushkin <yalex007@mail.ru>
2022-11-15 19:22:47 +0300

    Настройка cors сервера

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,11 +12,11 @@ module.exports.cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
   return next();
