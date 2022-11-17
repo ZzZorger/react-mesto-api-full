a2b323f4cec36646a7600e8b0d02a1ac6d24d303
Alexandr Yakushkin <yalex007@mail.ru>
2022-11-17 16:36:20 +0300

    Работа над карточками

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -15,7 +15,7 @@ module.exports.createCard = (req, res, next) => {
     link,
     owner: req.user._id,
   })
-    .then((card) => res.status(200).send({ data: card }))
+    .then((card) => res.status(200).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при создании карточки'));
